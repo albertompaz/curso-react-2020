7073924a1ec2d688a740f426f92dacaabb1cce5c
Alberto Paz <albertocmdp@gmail.com>
2021-06-07 08:44:20 -0300

    alteracoes feitas

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//function ComponenteFuncional() {
+ // return (
+  //  <h1>Hello</h1>
+  //)
+//}
+
+/*
+class App extends React.Component {
+
+  state = {
+    nome: ""
+  }
+
+  modificarNome = (event) => {
+    this.setState({
+      nome: event.target.value
+    })
+  }
+
+  criaComboBox = () => {
+    const opcoes = [ "Fulano", "Ciclano" ]
+    const comboBoxOpcoes = opcoes.map( opcao => <option>{opcao}</option> )
+
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
+  componentDidMount() {
+    console.log(`Executou o componentDidMount`)
+  }
+
+  render() {
+    console.log(`Executou o render`)
+    const MeuComboBox = () => this.criaComboBox()
+
+    return (
+      //<React.Fragment>
+      <>
+        <input type="text" value={this.state.nome} onChange={this.modificarNome}  />
+        <h1>Hello {this.state.nome}</h1>
+        <h1>Hello {this.props.nome} sua idade é: {this.props.idade} anos</h1>
+        <MeuComboBox />
+      </> 
+      //</React.Fragment>
+      )
+  }
+}
+*/
+
+function App(props) {
+
+  const modificarNome = event => {
+    console.log(event.target.value)
+  }
+
+  const criaComboBox = () => {
+    const opcoes = [ "Fulano", "Ciclano" ]
+    const comboBoxOpcoes = opcoes.map( opcao => <option>{opcao}</option> )
+
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+    const MeuComboBox = () => criaComboBox()
+
+    return (
+      //<React.Fragment>
+      <>
+        <input className="texto-centralizado" type="text" value={props.nome} onChange={modificarNome}  />
+        <h1>Hello {props.nome} sua idade é: {props.idade} anos</h1>
+        <MeuComboBox />
+      </> 
+      //</React.Fragment>
+      )
+  
 }
 
 export default App;
